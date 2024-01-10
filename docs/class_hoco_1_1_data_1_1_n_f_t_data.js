dd5b87d7f49933a63693102cac0d13c9ef0297b6
Dencho <denchyaknow@gmail.com>
2024-01-10 02:52:24 -0800

    Updated Docs

--- docs/class_hoco_1_1_data_1_1_n_f_t_data.js
+++ docs/class_hoco_1_1_data_1_1_n_f_t_data.js
@@ -1,7 +1,7 @@
 var class_hoco_1_1_data_1_1_n_f_t_data =
 [
+    [ "name", "class_hoco_1_1_data_1_1_n_f_t_data.html#a8ccf841cb59e451791bcb2e1ac4f1edc", null ],
     [ "description", "class_hoco_1_1_data_1_1_n_f_t_data.html#a23af17c78302b71c14ef38ea40b8d1d7", null ],
-    [ "external_url", "class_hoco_1_1_data_1_1_n_f_t_data.html#ae207b3df74200e77fae3389a9fc73029", null ],
     [ "image", "class_hoco_1_1_data_1_1_n_f_t_data.html#aca0304376f5c66f6d4a0b029b662d9b2", null ],
-    [ "name", "class_hoco_1_1_data_1_1_n_f_t_data.html#a8ccf841cb59e451791bcb2e1ac4f1edc", null ]
+    [ "external_url", "class_hoco_1_1_data_1_1_n_f_t_data.html#ae207b3df74200e77fae3389a9fc73029", null ]
 ];
--- docs/class_hoco_1_1_runtime_1_1_player_data.js
+++ docs/class_hoco_1_1_runtime_1_1_player_data.js
@@ -1,7 +1,7 @@
 var class_hoco_1_1_runtime_1_1_player_data =
 [
-    [ "Experience", "class_hoco_1_1_runtime_1_1_player_data.html#a72930a63e968cf646607de5f58e5fe68", null ],
-    [ "Level", "class_hoco_1_1_runtime_1_1_player_data.html#a879a93f9d2ab803bb4ca7f9bd55e52f8", null ],
     [ "PlayerAddress", "class_hoco_1_1_runtime_1_1_player_data.html#ad7d7a49ef737001922b4bb8f8e26ddf0", null ],
-    [ "PlayerName", "class_hoco_1_1_runtime_1_1_player_data.html#a7a928729abd5796564413abf828bf11a", null ]
+    [ "PlayerName", "class_hoco_1_1_runtime_1_1_player_data.html#a7a928729abd5796564413abf828bf11a", null ],
+    [ "Level", "class_hoco_1_1_runtime_1_1_player_data.html#a879a93f9d2ab803bb4ca7f9bd55e52f8", null ],
+    [ "Experience", "class_hoco_1_1_runtime_1_1_player_data.html#a72930a63e968cf646607de5f58e5fe68", null ]
 ];
